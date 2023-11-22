b13113d39997127e7abcd0706e0d38c12239b57a
cjc7122 <cjc7122@gmail.com>
2023-11-22 10:15:53 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ const sendVerificationEmail = async (req, res, next) => {
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.error('Error sending email:', error);
+				return res.status(402).json({ error: 'E-Mail failed to send!' });
             } else {
                 console.log('Email sent:', info.response);
             }
