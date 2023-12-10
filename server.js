62ef7c0c2d49c09c20919120b216b42d422f726a
cjc7122 <cjc7122@gmail.com>
2023-12-10 11:07:51 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -146,15 +146,15 @@ app.use(express.static('public'));
 
 // Login endpoint
 app.post('/login', authenticateUser, async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password1 } = req.body;
 
     try {
 		await client.connect();
 		
 		const db = client.db("MathBot");
 		const collection = db.collection("MathbotUsers");
-		console.log('Attempting to find user:', { email, password });
-		const credentials = await collection.findOne({ email, password});
+		console.log('Attempting to find user:', { email, password1 });
+		const credentials = await collection.findOne({ name: email, password: password1});
 		console.log('Credentials found:', credentials);
 	
 		if (credentials) {
