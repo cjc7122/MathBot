c3a57def62c43ab8a7d6a0f8a33f4b81d5b1ea6e
cjc7122 <cjc7122@gmail.com>
2023-12-10 10:27:16 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
-const MongoClient = require('mongodb');
-const ServerApiVersion = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 10000; // Update with your desired port
