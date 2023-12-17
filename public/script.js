23a17387312b800da9a35178649f380cfd236b0b
cjc7122 <cjc7122@gmail.com>
2023-12-16 22:12:02 -0600

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -424,9 +424,9 @@ function checkLoggedIn() {
 			document.getElementById('problem-input').style.display = 'none';
 			document.getElementById('solveButton').style.display = 'none';
 			document.getElementById('intro-text').style.display = 'none';
-			document.getElementById('WatchAd').style.display = 'block';
-			document.getElementById('Ad-Text').style.display = 'block';
-			document.getElementById('result').innerHTML = 'block';
+			document.getElementById('WatchAd').style.display = '';
+			document.getElementById('Ad-Text').style.display = '';
+			document.getElementById('result').innerHTML = '';
 		} else {
 			document.getElementById('GetCoin').style.display = 'block';
 		}
